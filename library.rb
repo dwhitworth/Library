41c09de22328b225b16d744f6b728744bd1a52b2
Donovan Whitworth <donovan.whitworth@gmail.com>
2014-02-07 14:08:38 -0600

    add books getter method to Library class

--- library.rb
+++ library.rb
@@ -32,11 +32,13 @@ class Borrower
 end
 
 class Library
-  def initialize(name)
+
+  def initialize(name = nil)
     @books = []
   end
 
   def books
+    @books
   end
 
   def add_book(title, author)
