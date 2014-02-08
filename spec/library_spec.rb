2b05865c50d6ca59c0654fffeee9054de15c94d9
Donovan Whitworth <donovan.whitworth@gmail.com>
2014-02-07 21:06:08 -0600

    fix returns borrowed books

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -180,20 +180,20 @@ describe Library do
     expect(lib.available_books.count).to eq(2)
   end
 
-  # it "returns borrowed books" do
-  #   lib = Library.new
-  #   lib.register_new_book("Eloquent JavaScript", "Marijn Haverbeke")
-  #   lib.register_new_book("Essential JavaScript Design Patterns", "Addy Osmani")
-  #   lib.register_new_book("JavaScript: The Good Parts", "Douglas Crockford")
+  it "returns borrowed books" do
+    lib = Library.new
+    lib.register_new_book("Eloquent JavaScript", "Marijn Haverbeke")
+    lib.register_new_book("Essential JavaScript Design Patterns", "Addy Osmani")
+    lib.register_new_book("JavaScript: The Good Parts", "Douglas Crockford")
 
-  #   # At first, no books are checked out
-  #   expect(lib.borrowed_books.count).to eq(0)
+    # At first, no books are checked out
+    expect(lib.available_books.count).to eq(3)
 
-  #   kors = Borrower.new("Michael Kors")
-  #   book = lib.check_out_book(lib.borrowed_books.first.id, kors)
+    kors = Borrower.new("Michael Kors")
+    book = lib.check_out_book(lib.available_books.first.id, kors)
 
-  #   # But now there should be one checked out book
-  #   expect(lib.borrowed_books.count).to eq(1)
-  #   expect(lib.borrowed_books.first).to be_a(Book)
-  # end
+    # But now there should be two books available
+    expect(lib.available_books.count).to eq(2)
+    expect(lib.available_books.first).to be_a(Book)
+  end
 end
